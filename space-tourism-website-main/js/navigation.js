ceb7294172dfb07baa2467a56fc0933f91eb71b4
Agungfir08 <agung.firmansyah@ui.ac.id>
2025-08-04 23:12:38 +0800

    finish the layout of destination page

---
+++ space-tourism-website-main/js/navigation.js
@@ -0,0 +1,22 @@
+//-----Navigation.js------
+
+const navigationButton = document.getElementById('navigation-button');
+const navigation = document.getElementById('navigation');
+
+function toggleNavigation() {
+    console.log('Toggle Navigation');
+    const buttonIcon = navigationButton.querySelector('img');
+    const isClosed = buttonIcon.src.includes('icon-hamburger.svg');
+
+    if (isClosed) {
+        buttonIcon.src = './assets/shared/icon-close.svg';
+        navigation.classList.remove('header__nav--closed');
+        navigationButton.setAttribute('aria-expanded', 'true');
+    } else {
+        buttonIcon.src = './assets/shared/icon-hamburger.svg';
+        navigation.classList.add('header__nav--closed');
+        navigationButton.setAttribute('aria-expanded', 'false');
+    }
+}
+console.log(navigationButton);
+navigationButton.addEventListener('click', toggleNavigation);
