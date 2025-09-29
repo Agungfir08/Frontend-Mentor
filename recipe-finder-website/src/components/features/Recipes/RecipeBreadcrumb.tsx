950329ec1489eb024a08fc268ed6a951299c63de
Agungfir08 <agung.firmansyah@ui.ac.id>
2025-09-29 22:51:17 +0800

    add Link from react router

--- recipe-finder-website/src/components/features/Recipes/RecipeBreadcrumb.tsx
+++ recipe-finder-website/src/components/features/Recipes/RecipeBreadcrumb.tsx
@@ -6,13 +6,16 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
+import { Link } from 'react-router';
 
 function RecipeBreadcrumb({ name }: { name: string }) {
     return (
         <Breadcrumb>
             <BreadcrumbList>
                 <BreadcrumbItem>
-                    <BreadcrumbLink href="/recipes">Recipes</BreadcrumbLink>
+                    <BreadcrumbLink asChild>
+                        <Link to="/recipes">Recipes</Link>
+                    </BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator />
                 <BreadcrumbItem>
